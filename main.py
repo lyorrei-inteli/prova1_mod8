85b37c67b60f30729555a40a38c0dc46fef5f889
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2023-11-17 15:42:20 -0300

    Mudança nos nomes das variáveis

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def order_status():
     print('Você quer saber como acompanhar o status do seu pedido.')
 
 # Dicionário de intenções em que a key representa uma expressão regular e o valor representa a ação que deve ser executada
-intent_dict = {
+intencoes = {
     r"(?:Como posso atualizar meu cartão de crédito?|Preciso mudar a forma de pagamento, o que fazer?|Quero atualizar minhas informações de pagamento|Método de pagamento desatualizado, como proceder para atualizar?)": "payment_update",
     r"(?:Onde vejo o status do meu pedido?|Como faço para rastrear meu pedido?|Quero saber onde está meu pedido|como faço?|Status de entrega|como consultar?)": "order_status"
 }
 
 # Dicionário de ações que faz um link da ação com a função que deve ser executada
-action_dict = {
+acoes = {
         "payment_update": payment_update,
         "order_status": order_status
 }
@@ -25,6 +25,10 @@ def main():
 
     # Loop que permite que o usuário digite comandos repetidamente
     while True:
+      
+      # Linha para separar pedidos no terminal
+      print('-------------------------------------')
+
       # Prompt de comando no terminal
       command = input("Digite o seu comando (caso deseje sair, digite \"sair\"): ")
 
@@ -36,7 +40,7 @@ def main():
       understood = False
       
       # Loop em cada expressão regular
-      for key, value in intent_dict.items():
+      for key, value in intencoes.items():
           
           # Checando se o comando que o usuário digitou faz correspondência com alguma expressâo regular
           pattern = re.compile(key)
@@ -46,7 +50,7 @@ def main():
             understood = True
 
             # Executa a função correspondente a aquela expressão
-            action_dict[value]()
+            acoes[value]()
 
       # Se nenhuma expressão regular corresponder ao comando do usuário, retornar um feedback
       if understood == False:
